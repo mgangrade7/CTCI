946302f978af7b34d0d92919a3b8a93ea17fa0ab
Mayank Gangrade <gangrademayank91@gmail.com>
2019-10-25 00:19:10 -0400

    "solution 1.6"

--- src/main/java/com/solution/ctci/chp1/solution6.java
+++ src/main/java/com/solution/ctci/chp1/solution6.java
@@ -4,8 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author gangrade.m
+String Compression: Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2blc5a3, If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+Hints: #92, if 110 
+
  */
 public class solution6 {
 
